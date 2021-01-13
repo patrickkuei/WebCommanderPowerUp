65010a5c07f428b728acfe72373d0945f7070750
Patrick.Yang <patrick.yang@varmeego.com>
2021-01-13 12:13:33 +0800

    add decorateFiles function

--- webcommanderpowerup/src/components/LeftNavbar.js
+++ webcommanderpowerup/src/components/LeftNavbar.js
@@ -5,23 +5,28 @@ import { FilesInfoContext } from "../contexts/FilesInfoContext";
 function LeftNavbar() {
   const { filesInfo, setFilesInfo } = useContext(FilesInfoContext);
 
-  const decoratedFolders = filesInfo.files.map((file, index) => {
+  const decorateFiles = (files) => {
     return (
-      <li className="nav-item" key={index.toString() + file}>
-        <a className="nav-link active" aria-current="page" href="#">
-          {file}
-        </a>
-      </li>
+      <ul className="navbar-nav">
+        {files.map((file) => {
+          return (
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                {file.name}
+              </a>
+              {file.subfolders && decorateFiles(file.subfolders)}
+            </li>
+          );
+        })}
+      </ul>
     );
-  });
+  };
 
   return (
     // Bootstrap LeftNavbar
     <div className="main__left-bar col-2 border">
       <nav className="main__left-bar__navigation navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <ul className="navbar-nav">{decoratedFolders}</ul>
-        </div>
+        <div className="container-fluid">{decorateFiles(filesInfo.files)}</div>
       </nav>
     </div>
   );
--- webcommanderpowerup/src/components/MainView.js
+++ webcommanderpowerup/src/components/MainView.js
@@ -7,9 +7,9 @@ function MainView() {
   const { filesInfo, setFilesInfo } = useContext(FilesInfoContext);
   return (
     <div className="main__main-view row overflow-auto">
-      {filesInfo.files.map((file, index) => (
+      {/* {filesInfo.files.map((file, index) => (
         <MainViewItem key={index.toString() + file} file={file} />
-      ))}
+      ))} */}
     </div>
   );
 }
