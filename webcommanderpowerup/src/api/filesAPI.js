a5290967586d6db34e55895937e931a4cad19763
Patrick.Yang <patrick.yang@varmeego.com>
2021-02-05 17:24:42 +0800

    refactor: api function

--- webcommanderpowerup/src/api/filesAPI.js
+++ webcommanderpowerup/src/api/filesAPI.js
@@ -2,51 +2,18 @@ import axios from "axios";
 import { BASE_URL } from "../constants";
 
 const api = {
-  getFolderHierarchy: () => {
-    return axios.get(`${BASE_URL}/hierarchy`);
-  },
+  getFolderHierarchy: () => axios.get(`${BASE_URL}/hierarchy`),
+  getFilesById: (folderId) => axios.get(`${BASE_URL}/items/${folderId}`),
+  createFileByData: (parentItemId, items, folders) =>
+    axios.post(`${BASE_URL}/items/`, {
+      payload: { parentItemId, items, folders },
+    }),
+  pasteFilesById: (folder, files) =>
+    axios.post(`${BASE_URL}/items/`, { payload: { folder, files } }),
 
-  getFilesById: (folderId) => {
-    return axios.get(`${BASE_URL}/items/${folderId}`);
-  },
-
-  createFileByData: (parentItemId, items, folders) => {
-    return axios({
-      url: `${BASE_URL}/items/`,
-      method: "POST",
-      transformRequest: [
-        ...axios.defaults.transformRequest,
-        (data) => ({ payload: data }),
-      ],
-      data: {
-        parentItemId: parentItemId,
-        items: items,
-        folders: folders,
-      },
-    });
-  },
-
-  pasteFilesById: (folder, files) => {
-    return axios({
-      url: `${BASE_URL}/items/`,
-      method: "POST",
-      transformRequest: [
-        ...axios.defaults.transformRequest,
-        (data) => ({ payload: data }),
-      ],
-      data: {
-        targetFolder: folder,
-        files: files,
-      },
-    });
-  },
-
-  deleteFileById: (fileId) => {
-    return axios({
-      url: `${BASE_URL}/items/`,
-      method: "DELETE",
-      params: { id: fileId },
-    });
-  },
+  deleteFileById: (fileId) =>
+    axios.delete(`${BASE_URL}/items/`, {
+      params: { fileId },
+    }),
 };
 export default api;
