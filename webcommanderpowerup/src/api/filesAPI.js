061e8f4f016c16dbc99703ddf4936e8123ca088a
Patrick.Yang <patrick.yang@varmeego.com>
2021-08-18 12:31:47 +0800

    update

--- webcommanderpowerup/src/api/filesAPI.js
+++ webcommanderpowerup/src/api/filesAPI.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 import { BASE_URL } from "../constants";
 
+const instace = axios.create({ baseURL: `${BASE_URL}/files` });
+
 const api = {
-  getFolderHierarchy: () => axios.get(`${BASE_URL}/hierarchy`),
-  getFilesById: (folderId) => axios.get(`${BASE_URL}/items/${folderId}`),
+  getFolderHierarchy: () => instace.get(`/hierarchy`),
+  getFilesById: (folderId) =>
+    axios.get(`${FILE_API_BASE_URL}/items/${folderId}`),
   createFileByData: (parentItemId, items, folders) =>
-    axios.post(`${BASE_URL}/items/`, {
+    axios.post(`${FILE_API_BASE_URL}/items/`, {
       payload: { parentItemId, items, folders },
     }),
   pasteFilesById: (folder, files) =>
-    axios.post(`${BASE_URL}/items/`, { payload: { folder, files } }),
+    axios.post(`${FILE_API_BASE_URL}/items/`, { payload: { folder, files } }),
 
   deleteFileById: (fileId) =>
-    axios.delete(`${BASE_URL}/items/`, {
+    axios.delete(`${FILE_API_BASE_URL}/items/`, {
       params: { fileId },
     }),
 };
